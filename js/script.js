89fd42b4ae7e95890d564a57b0ad63bec121222c
edgarssevcovs <50653823+edgarssevcovs@users.noreply.github.com>
2019-06-04 22:23:05 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,66 +13,36 @@ function innit () {
 }
 
 function onClick() {
-    console.log('Button hit');
     onClickClear();
+    console.log('Button hit');
         for(let i = 1; i<101; i++){
             const element = document.createElement('div');
             element.className="element" + i;
             elements.appendChild(element);
                 if (i % 3 ===0 && i % 5 === 0){
-                    element.style.display="grid";
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
-                    element.style.backgroundColor="#62ff32";
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="FizzBuzz " + element.className;
-                    element.style.textAlign="center";
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "FizzBuzz");
+                    element.innerText="FizzBuzz " + element.id;
                 }
                 else if (i % 3 === 0 && i % 5 !== 0) {
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
-                    element.style.backgroundColor="#ff3262";
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="Fizz " + element.className;
-                    element.style.textAlign="center";
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Fizz");
+                    element.innerText="Fizz " + element.id;
+                    
                 }
 
                 else if (i % 5 === 0 && i % 3 !== 0){
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
-                    element.style.backgroundColor="#ff8132";
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="Buzz " + element.className;
-                    element.style.textAlign="center";
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Buzz");
+                    element.innerText="Buzz " + element.id;
+                    
                 }
 
                 else if (i % 3 !== 0 && i % 5 !== 0){
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
-                    element.style.backgroundColor="#3292ff";
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText=element.className;
-                    element.style.textAlign="center";
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Empty");
+                    element.innerText=element.id;
+                    
                 }
         }
 }
@@ -89,7 +59,7 @@ function onClickCustom (){
     console.log('Divs cleared');
     console.log('Generate custom divs');
     var startValue = document.getElementById('startValue').value;
-    var endValue = document.getElementById('endValue').value;
+    var endValue = document.getElementById('endValue').value;   
     var fizzValue = document.getElementById('fizzValue').value;
     var buzzValue = document.getElementById('buzzValue').value;
     var fizzColor = document.getElementById('fizzColor').value;
@@ -106,62 +76,35 @@ function onClickCustom (){
     console.log('none color - ' + noneColor);
         for (startValue; startValue<=endValue; startValue++){
             const element = document.createElement('div');
-            element.className="element" + startValue;
+            element.id="element" + startValue;
+            
             elements.appendChild(element);
                 if (startValue % fizzValue === 0 && startValue % buzzValue === 0){
-                    element.style.display="grid";
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "FizzBuzz");
+                    element.innerText="FizzBuzz " + element.id;
                     element.style.backgroundColor=fizzBuzzColor;
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="FizzBuzz " + element.className;
-                    element.style.textAlign="center";
                 }
                 else if (startValue % fizzValue === 0 && startValue % buzzValue !== 0) {
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Fizz");
+                    element.innerText="Fizz " + element.id;
                     element.style.backgroundColor=fizzColor;
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="Fizz " + element.className;
-                    element.style.textAlign="center";
                 }
 
                 else if (startValue % buzzValue === 0 && startValue % fizzValue !== 0){
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Buzz");
+                    element.innerText="Buzz " + element.id;
                     element.style.backgroundColor=buzzColor;
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText="Buzz " + element.className;
-                    element.style.textAlign="center";
                 }
 
                 else if (startValue % fizzValue !== 0 && startValue % buzzValue !== 0){
-                    element.style.width="90px";
-                    element.style.height="90px";
-                    element.style.margin="10px";
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Empty");
+                    element.innerText=element.id;
                     element.style.backgroundColor=noneColor;
-                    element.style.color="white";
-                    element.style.borderRadius="8px";
-                    element.style.borderStyle="solid";
-                    element.style.borderWidth="1px";
-                    element.style.borderColor="#000";
-                    element.innerText=element.className;
-                    element.style.textAlign="center";
+
                 }
         }
 }
